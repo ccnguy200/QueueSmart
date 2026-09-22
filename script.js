f87385e87c466da62652677ca583b1131f2b485b
csjensen16 <csjensen16@gmail.com>
2026-09-22 16:33:13 -0500

    Fixed Register Logic
    * Fixed register logic and storage
    * Added reset to login and register

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
-let users = JSON.parse(localStorage.getItem("users")) || {"example@gmail.com": "password"};
+let users = JSON.parse(localStorage.getItem("users")) || 
+{
+    "admin@email.com": {password: "password", role: "admin"},
+    "user@email.com": {password: "password", role: "user"}
+};
 
 function login() {
     let username = document.getElementById("email").value;
     let password = document.getElementById("secret").value;
-    if((users[username] == password)) {
+    if(users[username] && (users[username].password === password)) {
         alert("Login Successful");
-        return true;
-    }
-    else {
-        alert("Incorrect Login");
+        if(users[username].role == "admin"){
+            window.location.href = "admin_dashboard.html";
+        }
+        else{
+            window.location.href = "user_dashboard.html";
+        }
         return false;
     }
+    alert("Incorrect Login");
+    return false;
 }
 
 function register() {
     let first = document.getElementById("first").value;
     let second = document.getElementById("second").value;
+    let role = document.getElementById("role").value;
 
     if(first in users){
         alert("That email is already registered with an account!")
         return false;
     }
-    else {
-        alert("Register Successful")
-        users[first] = second;
-        localStorage.setItem("users", JSON.stringify(users));
-        return true;
+
+    if(role == "admin"){
+        let cardNumber = document.getElementById("cardNumber").value;
+        let cardExpiry = document.getElementById("cardExpiry").value;
+        let cardCVV = document.getElementById("cardCVV").value;
+
+        if(!cardNumber || !cardExpiry || !cardCVV){
+            alert("Admins must provide credit card information!");
+            return false;
+        }
     }
+
+    users[first] = {password: second, role: role};
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("Register Successful")
+    return false;
+}
+
+function adminRegister() {
+    let role = document.getElementById("role").value;
+
+    if(role === "admin"){
+        document.getElementById("adminReg").style.display = "block";
+    }
+    else {
+        document.getElementById("adminReg").style.display = "none";
+    }
+}
+
+function resetData() {
+    localStorage.removeItem("users");
+    alert("Data reset. Default accounts restored on next load.");
+    location.reload();
 }
